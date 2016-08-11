fa90a85ea54ba06315ebd9b4da83aff64ca79a16
zhenzhen <1147865669@qq.com>
2016-08-11 19:59:20 +0800

    文章分类增删改查完毕

---
+++ MyBlog.Model/Enum/ArticleTypeEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlog.Model.Enum
+{
+    public enum ArticleTypeEnum
+    {
+        //根节点
+        ParentId=0
+    }
+}
--- MyBlog.WebUI/Controllers/ArticleTypeController.cs
+++ MyBlog.WebUI/Controllers/ArticleTypeController.cs
@@ -5,26 +5,87 @@ using System.Web;
 using System.Web.Mvc;
 using MyBlog.IBLL;
 using MyBlog.Model;
+using MyBlog.Model.Enum;
+using MyBlog.WebUI.Filter;
 using Newtonsoft.Json;
 
 namespace MyBlog.WebUI.Controllers
 {
+    [IsAdminActionFilter]//管理员才有权限对文章分类进行操作
     public class ArticleTypeController : Controller
     {
         private IArticleTypeService ArticleTypeService = BLLContainer.Container.Resolve<IArticleTypeService>();
         // GET: ArticleType
+
         public ActionResult Index()
         {
-            var zNodes = ArticleTypeService.GetModels(p => true).Select(p=>new
+            //  { id: 1, pId: 0, name: "[core] 基本功能 演示", open: true},
+            var zNodes = ArticleTypeService.GetModels(p => true).Select(p => new
             {
-                id=p.Id,
-                pId=p.ParentId,
-                name =p.Title,
-                open=true
+                id = p.Id,
+                pId = p.ParentId,
+                name = p.Title,
+                open = true
             });
             ViewBag.zNodes = JsonConvert.SerializeObject(zNodes);
-                //  { id: 1, pId: 0, name: "[core] 基本功能 演示", open: true},
             return View();
         }
+
+        #region 添加文章分类
+        public ActionResult Add(ArticleType articleType)
+        {
+            if (ArticleTypeService.Add(articleType))
+            {
+                return Json(new { status = "ok", msg = "添加成功" }, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return Json(new { status = "no", msg = "添加失败" }, JsonRequestBehavior.AllowGet);
+            }
+        }
+        #endregion
+
+        #region 更新文章分类
+        public ActionResult Update(int id)
+        {
+            ArticleType articleType= ArticleTypeService.GetModels(p => p.Id == id).FirstOrDefault();
+            return Json(articleType,JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        public ActionResult Update(ArticleType articleType)
+        {
+            if (ArticleTypeService.Update(articleType))
+            {
+                return Json(new { status = "ok", msg = "修改成功" }, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return Json(new { status = "no", msg = "修改失败" }, JsonRequestBehavior.AllowGet);
+            }
+        }
+        #endregion
+
+        #region 删除文章分类
+        public ActionResult Delete(int id)
+        {
+            ArticleType articleType = ArticleTypeService.GetModels(p => p.Id == id).FirstOrDefault();
+            if (articleType!=null)
+            {
+                if (ArticleTypeService.Delete(articleType))
+                {
+                    return Json(new { status = "ok", msg = "删除成功" }, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    return Json(new { status = "no", msg = "删除失败" }, JsonRequestBehavior.AllowGet);
+                }
+            }
+            else
+            {
+                return Json(new { status = "no", msg = "文章分类不存在" }, JsonRequestBehavior.AllowGet);
+            }
+            
+        }
+        #endregion
     }
 }
