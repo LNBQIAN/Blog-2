2e3075da7eaf4881e2aa0d8cf0af3e2d04ad44e4
zhenzhen <1147865669@qq.com>
2016-08-15 20:54:30 +0800

    完成了文章的增删改查

--- MyBlog.WebUI/Controllers/ArticleInfoController.cs
+++ MyBlog.WebUI/Controllers/ArticleInfoController.cs
@@ -17,6 +17,8 @@ namespace MyBlog.WebUI.Controllers
     public class ArticleInfoController : Controller
     {
         private IArticleInfoService ArticleInfoService = BLLContainer.Container.Resolve<IArticleInfoService>();
+        private IArticleTypeService ArticleTypeService = BLLContainer.Container.Resolve<IArticleTypeService>();
+        private IUserInfoService UserInfoService = BLLContainer.Container.Resolve<IUserInfoService>();
         // GET: ArticleInfo
         #region 后台管理文章首页（管理员访问）
         [IsAdminActionFilter]
@@ -49,18 +51,18 @@ namespace MyBlog.WebUI.Controllers
             {
                 pageIndex = pageCount;
             }
-            var articleList = ArticleInfoService.GetModelsByPage(pageSize, pageIndex, false, p => p.Id, p => true).Select(p=>new
+            var articleList = ArticleInfoService.GetModelsByPage(pageSize, pageIndex, false, p => p.Id, p => true).Select(p => new
             {
-                Id=p.Id,
-                ArticleTypeName=p.ArticleType.Title,
-                Title=p.ArticleTitle,
+                Id = p.Id,
+                ArticleTypeName = p.ArticleType.Title,
+                Title = p.ArticleTitle,
                 UserInfo = p.UserInfo,
-                ReadCount=p.ReadCount,
-                PubTime=p.PubTime
+                ReadCount = p.ReadCount,
+                PubTime = p.PubTime
             });
             string pageBar = PageBar.GetNumberPageBarWithFirstIndexAndLastIndex(pageCount, pageIndex);
 
-            return Content(JsonConvert.SerializeObject(new {articleList = articleList, pageBar = pageBar}));
+            return Content(JsonConvert.SerializeObject(new { articleList = articleList, pageBar = pageBar }));
         }
         #endregion
 
@@ -73,7 +75,7 @@ namespace MyBlog.WebUI.Controllers
             List<int> idList = temp.Select(p => Convert.ToInt32(p)).ToList();
             if (ArticleInfoService.DeleteList(idList))
             {
-                return Json(new { status ="ok",msg="删除成功"}, JsonRequestBehavior.AllowGet);
+                return Json(new { status = "ok", msg = "删除成功" }, JsonRequestBehavior.AllowGet);
             }
             else
             {
@@ -95,9 +97,9 @@ namespace MyBlog.WebUI.Controllers
         public ActionResult Add(ArticleInfo articleInfo)
         {
             //获取当前登录用户id
-            if (Session["UserInfo"]==null)
+            if (Session["UserInfo"] == null)
             {
-                return Json(new {status="no",msg="未登录"},JsonRequestBehavior.AllowGet);
+                return Json(new { status = "no", msg = "未登录" }, JsonRequestBehavior.AllowGet);
             }
             articleInfo.UserId = (Session["UserInfo"] as UserInfo).Id;
             articleInfo.PubTime = DateTime.Now;
@@ -110,6 +112,33 @@ namespace MyBlog.WebUI.Controllers
             else
             {
                 return Json(new { status = "no", msg = "添加失败" }, JsonRequestBehavior.AllowGet);
+            }
+        }
+        #endregion
+
+        #region 修改(管理员访问)
+        [IsAdminActionFilter]
+        public ActionResult Edit(int id)
+        {
+            var articleInfo = ArticleInfoService.GetModels(p => p.Id == id).FirstOrDefault();
+            ViewBag.articleTypeInfo = ArticleTypeService.GetModels(p => p.Id == articleInfo.ArticleTypeId).FirstOrDefault();
+            ViewBag.userInfo = UserInfoService.GetModels(p => p.Id == articleInfo.UserId).FirstOrDefault();
+            return View(articleInfo);
+        }
+
+        [IsAdminActionFilter]
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Edit(ArticleInfo articleInfo)
+        {
+            articleInfo.PubTime = DateTime.Now;
+            if (ArticleInfoService.Update(articleInfo))
+            {
+                return Json(new { status = "ok", msg = "修改成功" }, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return Json(new { status = "no", msg = "修改失败" }, JsonRequestBehavior.AllowGet);
             }
         }
         #endregion
@@ -135,33 +164,33 @@ namespace MyBlog.WebUI.Controllers
             }
             if (!extTable.ContainsKey(dir))
             {
-                return Json(new { error = 1, message = "文件格式不正确" },JsonRequestBehavior.AllowGet);
+                return Json(new { error = 1, message = "文件格式不正确" }, JsonRequestBehavior.AllowGet);
             }
 
             if (imgFile == null)
             {
-                return Json(new { error=1, message="上传文件大小超过限制"}, JsonRequestBehavior.AllowGet);
+                return Json(new { error = 1, message = "上传文件大小超过限制" }, JsonRequestBehavior.AllowGet);
             }
             string fileName = imgFile.FileName;
             string fileExt = Path.GetExtension(fileName).ToLower();
             if (String.IsNullOrEmpty(fileExt) || Array.IndexOf(((String)extTable[dir]).Split(','), fileExt.Substring(1).ToLower()) == -1)
             {
-                return Json(new { error = 1, message = "上传文件扩展名是不允许的扩展名。\n只允许" + ((String)extTable[dir])+"格式" }, JsonRequestBehavior.AllowGet);
+                return Json(new { error = 1, message = "上传文件扩展名是不允许的扩展名。\n只允许" + ((String)extTable[dir]) + "格式" }, JsonRequestBehavior.AllowGet);
             }
             //创建文件夹
             //获取当前登录用户id
             if (Session["UserInfo"] == null)
             {
-                return Json(new {error = 1, message = "未登录"}, JsonRequestBehavior.AllowGet);
+                return Json(new { error = 1, message = "未登录" }, JsonRequestBehavior.AllowGet);
             }
-            string dirPath = "/UserFile/用户编号" + (Session["UserInfo"] as UserInfo).Id + "/"+dir+"/";
+            string dirPath = "/UserFile/用户编号" + (Session["UserInfo"] as UserInfo).Id + "/" + dir + "/";
             if (!Directory.Exists(Request.MapPath(dirPath)))
             {
                 //不存在就创建
                 Directory.CreateDirectory(Request.MapPath(dirPath));
             }
             string newFileName = DateTime.Now.ToString("yyyyMMddHHmmss_ffff") + fileExt;
-            imgFile.SaveAs(Request.MapPath(dirPath+newFileName));
+            imgFile.SaveAs(Request.MapPath(dirPath + newFileName));
             //判断保存的文件是否存在
             if (System.IO.File.Exists(Request.MapPath(dirPath + newFileName)))
             {
